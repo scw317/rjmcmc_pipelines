a36c76950b3a0d7eac6595ce6901b74efd08737a
ChanwooSong <scw3917@gmail.com>
2026-03-30 03:08:36 +0900

    Snapshot: Add mean estimation.

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import pandas as pd
 # %% Data
 # ++++++++++++++++++++++++++++++++
 
-data_path = Path.home() / "Dropbox/Material/Coauthor/_backup/Kang S. et al., 2026/analysis/ovro.txt"
+data_path = Path.home() / "Dropbox/Material/Coauthor/_backup/Kang S. et al., 2026/analysis/bu.txt"
 data = np.loadtxt(data_path, comments="#")
 
-save_dir = Path.home() / "Dropbox/Material/Coauthor/_backup/Kang S. et al., 2026/analysis/ovro/1"
+save_dir = Path.home() / "Dropbox/Material/Coauthor/_backup/Kang S. et al., 2026/analysis/bu/7"
 save_dir.mkdir(parents=True, exist_ok=True)
 
 # ================================
@@ -21,14 +21,14 @@ save_dir.mkdir(parents=True, exist_ok=True)
 
 # The limit number of model components
 n_dim_min = 1
-n_dim_max = 50
+n_dim_max = 30
 
 # The final number of posterior samples is
 # (The number of T=1 chains) * (n_iterations - brunin_iterations) / save_every.
-n_chains = 100
-n_iterations = 2000000
-burnin_iterations = 1500000
-save_every = 200
+n_chains = 32
+n_iterations = 1500000
+burnin_iterations = 1000000
+save_every = 100
 verbose = True
 print_every = 100000
 
@@ -197,7 +197,10 @@ for dim in np.unique(n_dims):
     # Estimate parameters
     est_results["estimates"][dim] = {}
     for tn in trans_param_names:
-        est_results["estimates"][dim][tn] = np.percentile(est_results["samples"][dim][tn], [50, 15.87, 84.13], axis=0)
+        est_results["estimates"][dim][tn] = np.vstack((
+            np.mean(est_results["samples"][dim][tn], axis=0),
+            np.percentile(est_results["samples"][dim][tn], [50, 15.87, 84.13], axis=0),
+        ))
     
     # For forward model values
     fwd_vals = []
@@ -205,7 +208,10 @@ for dim in np.unique(n_dims):
         fwd_vals.append(results["target.dpred"][i])
     fwd_vals = np.vstack(fwd_vals)
     est_results["samples"][dim]["forward"] = fwd_vals
-    est_results["estimates"][dim]["forward"] = np.percentile(fwd_vals, [50, 15.87, 84.13], axis=0)
+    est_results["estimates"][dim]["forward"] = np.vstack((
+        np.mean(fwd_vals, axis=0),
+        np.percentile(fwd_vals, [50, 15.87, 84.13], axis=0),
+        ))
 
 # For fixed parameters
 for fn in fixed_param_names:
@@ -213,10 +219,25 @@ for fn in fixed_param_names:
     for i in idxs:
         samples.append(results[f"fixed_space.{fn}"][i].item())
     est_results["samples"][fn] = np.array(samples)     
-    est_results["estimates"][fn] = np.percentile(est_results["samples"][fn], [50, 15.87, 84.13], axis=0)
+    est_results["estimates"][fn] = np.hstack((
+        np.mean(est_results["samples"][fn], axis=0),
+        np.percentile(est_results["samples"][fn], [50, 15.87, 84.13], axis=0),
+        ))
 
 # ================================
-# %% Export to excel
+# %% Export samples to txt
+# ++++++++++++++++++++++++++++++++
+
+for dim in np.unique(n_dims):
+    for tn in trans_param_names:
+        np.savetxt(save_dir / f"trans_{dim}.txt", est_results["samples"][dim][tn])
+    np.savetxt(save_dir / f"fwd_{dim}.txt", est_results["samples"][dim]["forward"])
+
+for fn in fixed_param_names:
+    np.savetxt(save_dir / "fixed.txt", est_results["samples"][fn])
+
+# ================================
+# %% Export estimates to excel
 # ++++++++++++++++++++++++++++++++
 
 with pd.ExcelWriter(save_dir / "results.xlsx") as w:
@@ -224,7 +245,7 @@ with pd.ExcelWriter(save_dir / "results.xlsx") as w:
     dim_df = pd.DataFrame(data=np.vstack(np.unique(n_dims, return_counts=True)).T, columns=["dim", "num"])
     dim_df.to_excel(w, sheet_name="dim", index=False)
     # Fixed parameter sheet
-    est_type_col_df = pd.DataFrame(data=["point", "lower", "upper"], columns=["est_type"])
+    est_type_col_df = pd.DataFrame(data=["mean", "median", "lower", "upper"], columns=["est_type"])
     param_col_df = pd.DataFrame(
         data=np.hstack([est_results["estimates"][fn][..., np.newaxis] for fn in fixed_param_names]),
         columns=fixed_param_names,
@@ -234,9 +255,9 @@ with pd.ExcelWriter(save_dir / "results.xlsx") as w:
     # Trans parameter sheet
     trand_df_ls = []
     for dim in np.unique(n_dims):
-        dim_col_df = pd.DataFrame(np.repeat(dim, dim * 3), columns=["dim"])
-        seq_col_df = pd.DataFrame(np.tile(np.arange(dim), 3), columns=["seq"])
-        est_type_col_df = pd.DataFrame(np.repeat(["point", "lower", "upper"], dim), columns=["est_type"])
+        dim_col_df = pd.DataFrame(np.repeat(dim, dim * 4), columns=["dim"])
+        seq_col_df = pd.DataFrame(np.tile(np.arange(dim), 4), columns=["seq"])
+        est_type_col_df = pd.DataFrame(np.repeat(["mean", "median", "lower", "upper"], dim), columns=["est_type"])
         param_col_df = pd.DataFrame(
             data=np.hstack([np.hstack(est_results["estimates"][dim][tn])[..., np.newaxis] for tn in trans_param_names]),
             columns=trans_param_names,
@@ -296,17 +317,18 @@ plot_data = np.linspace(data[:, 0].min(), data[:, 0].max(), 1000)
 for dim in np.unique(n_dims):    
     fig, ax = plt.subplots(dpi=300)
     ax.errorbar(data[:, 0], data[:, 1], data[:, 2], ls="none", marker=".", color="black", label="Data")
-    point, lower, upper = est_results["estimates"][dim]["forward"]
-    ax.plot(data[:, 0], point, lw=1, color="red", label="Model")
+    mean, median, lower, upper = est_results["estimates"][dim]["forward"]
+    ax.plot(data[:, 0], mean, lw=1.5, color="red", label="Mean forward")
+    ax.plot(data[:, 0], median, lw=1, color="orange", label="Median forward")
     ax.fill_between(data[:, 0], lower, upper, color="red", alpha=0.3, label=r"$1\sigma$ CI")
     model_val = model_func(
         plot_data,
-        *[est_results["estimates"][dim][tn][2][..., np.newaxis] for t, tn in enumerate(trans_param_names)],
+        *[est_results["estimates"][dim][tn][0][..., np.newaxis] for t, tn in enumerate(trans_param_names)],
     ) + est_results["estimates"]["fqs"][0]
-    ax.plot(plot_data, model_val.T, lw=0.5, color="orange")
+    ax.plot(plot_data, model_val.T, lw=0.5, color="violet")
     ax.legend()
     ax.set_xlabel("MJD")
     ax.set_ylabel("Flux density [Jy]")
     ax.set_title(f"Data and model plot (dim={dim})")
-    #fig.savefig(save_dir / f"model_{dim}.png")
+    fig.savefig(save_dir / f"model_{dim}.png")
     
